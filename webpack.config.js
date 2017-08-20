1769f67300b97ef8902766e174406d0fc5b74d6c
李刚 <515930926@qq.com>
2017-08-20 19:30:47 +0800

    Loading Data

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,19 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    'url-loader'
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
                 ]
             }
         ]
