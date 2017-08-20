47b0a5a792b7842f0d12924510ea54b67eb04d18
李刚 <515930926@qq.com>
2017-08-20 10:08:40 +0800

    creating a bundle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import _ from 'lodash';
+
 function component() {
     var element = document.createElement('div');
 
+    // Lodash, now impored by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
